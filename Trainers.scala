73153a533b6567b7fb82b87d6b8570ccaf008a48
Jakobbengtsson <34511629+Jakobbengtsson@users.noreply.github.com>
2017-12-14 09:15:14 +0100

    Update Trainers.scala

--- Trainers.scala
+++ Trainers.scala
@@ -11,7 +11,7 @@ object Trainers {
     val party: Array[Pokemon] = Array((new Skarmory(57, "Skarmory")), (new Claydol(55, "Claydol")), (new Aggron(56, "Aggron")), (new Cradily(56, "Cradily")), (new Armaldo(56, "Armaldo")), (new Metagross(58, "Metagross")))
   }
   val player = new Trainer("You") {
-    val party:Array[Pokemon] = Array((new Charizard(60, "Charizard")) ,(new Charizard(60, "Charizard")), (new Charizard(60, "Charizard")), (new Charizard(60, "Charizard")), (new Charizard(60, "Charizard")), (new Charizard(60, "Charizard")))
+    val party:Array[Pokemon] = Array((new Blaziken(58, "Blaziken")) ,(new Walrein(60, "Walrein")), (new Swampert(57, "Swampert")), (new Salamence(56, "Salamence")), (new Charizard(55, "Charizard")), (new Pikachu(80, "Pikachu")))
   }
 }
 
